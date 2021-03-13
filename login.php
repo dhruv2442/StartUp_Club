b6fe9136ac7aa09407862af5d5f7d5d8c4502a52
Deep Shah <dshah0405@gmail.com>
2021-03-14 04:29:57 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php
+    session_start();
+    include('config.php');
+    if (isset($_POST['login'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $query = $connection->prepare("SELECT * FROM users WHERE username=:username");
+        $query->bindParam("username", $username, PDO::PARAM_STR);
+        $query->execute();
+        $result = $query->fetch(PDO::FETCH_ASSOC);
+        if (!$result) {
+            echo '<p class="error">Username password combination is wrong!</p>';
+        } else {
+            if (password_verify($password, $result['password'])) {
+                $_SESSION['user_id'] = $result['id'];
+                echo '<p class="success">Congratulations, you are logged in!</p>';
+                header("Location: random.html"); 
+                // exit;
+            } else {
+                echo '<p class="error">Username password combination is wrong!</p>';
+            }
+        }
+    }
+?>
