bff36b7b1b82c79013ddd08776afa02c7a1a94b9
Deep Shah <dshah0405@gmail.com>
2021-03-13 22:02:22 +0530

    Add files via upload
    indextologin and deep.js changed

--- deep.js
+++ deep.js
@@ -17,25 +17,3 @@ const options = {
   darkmode.showWidget();
 
 
-  document.addEventListener('DOMContentLoaded', () => {
-
-    const themeStylesheet = document.getElementById('theme');
-    const storedTheme = localStorage.getItem('theme');
-    if(storedTheme){
-        themeStylesheet.href = storedTheme;
-    }
-    const themeToggle = document.getElementById('theme-toggle');
-    themeToggle.addEventListener('click', () => {
-        // if it's light -> go dark
-        if(themeStylesheet.href.includes('light')){
-            themeStylesheet.href = 'dark-theme.css';
-            themeToggle.innerText = 'Switch to light mode';
-        } else {
-            // if it's dark -> go light
-            themeStylesheet.href = 'light-theme.css';
-            themeToggle.innerText = 'Switch to dark mode';
-        }
-        // save the preference to localStorage
-        localStorage.setItem('theme',themeStylesheet.href)  
-    })
-})
